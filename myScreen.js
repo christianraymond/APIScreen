b3384482a343946c92d435163abc160d3cd3a4d9
christianraymond <ombenisn.raymond@gmail.com>
2017-05-18 12:27:33 +0200

    api screen searching

---
+++ myScreen.js
@@ -0,0 +1,23 @@
+var lookupButton = document.querySelector('.lookup');
+var userName = document.querySelector('#userName');
+var output = document.querySelector('.output');
+
+
+lookupButton.addEventListener('click', function(){
+
+    var username = userName.value;
+
+    var url = "http://api.github.com/users/" + username + '/repos'
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+        heades: {'Authorization': "https://api.github.com/users/christianraymond/repos"}
+    })
+    .then(function(result){
+        // var message = 'Hi ' + username + ' you have ' + result.public_repos + ' in Github'
+         var message = "Hi " + username + " you have " + JSON.stringify(result) + " repos";
+        output.innerHTML = message;
+        console.log(message);
+    });
+});
