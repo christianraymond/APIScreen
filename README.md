61045d3dd6a248f9ac263d16e9b3b6c86e4bd615
christianraymond <ombenisn.raymond@gmail.com>
2017-05-23 10:55:35 +0200

    $.ajax getting github username info

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # APIScreen
+This repos allows user to search their github repos.
+User enter their github username, then the pages prompts the message to the screen.
+The app count the amount of repositories the user has in his(her) github account.
+It'a an API(Application Protocol Interface)request taking place here.  
--- myScreen.js
+++ myScreen.js
@@ -7,7 +7,7 @@ lookupButton.addEventListener('click', function(){
 
     var username = userName.value;
 
-    var url = "http://api.github.com/users/" + username + '/repos'
+    var url = "http://api.github.com/users/" + username 
 
     $.ajax({
         type: 'GET',
@@ -15,8 +15,8 @@ lookupButton.addEventListener('click', function(){
         heades: {'Authorization': "https://api.github.com/users/christianraymond/repos"}
     })
     .then(function(result){
-        // var message = 'Hi ' + username + ' you have ' + result.public_repos + ' in Github'
-         var message = "Hi " + username + " you have " + JSON.stringify(result) + " repos";
+         var message = 'Hi, ' + username + ', you have ' + result.public_repos + ' github repositories in Github.'
+//         var message = "Hi " + username + " you have " + JSON.stringify(result) + " repos";
         output.innerHTML = message;
         console.log(message);
     });
